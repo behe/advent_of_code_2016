eda1523b24e62f379194b348ab1b844883c3c44a
Johan Lind <johan.behe.lind@gmail.com>
2016-12-16 07:16:40 +0100

    Finished day 4 part 1

---
+++ test/advent_of_code_04_test.exs
@@ -0,0 +1,59 @@
+defmodule AdventOfCode04Test do
+  use ExUnit.Case
+
+  test "aaaaa-bbb-z-y-x-123[abxyz] is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically" do
+    assert real_room?("aaaaa-bbb-z-y-x-123[abxyz]") == true
+  end
+
+  def real_room?(room) do
+    {name, _id, checksum} = extract_room_details(room)
+    checksum(name) == checksum
+  end
+
+  def extract_room_details(room) do
+    [[_, name, id, checksum]] = Regex.scan(~r/(.*)-(.*)\[(\w{5})\]/, room)
+    {name, id, checksum}
+  end
+
+  def checksum(name) do
+    name
+    |> String.replace("-", "")
+    |> String.split("")
+    |> Enum.slice(0..-2)
+    |> Enum.group_by(&(&1))
+    |> Enum.sort_by(fn({_, elements}) -> length(elements) end, &>=/2)
+    |> Enum.map(fn({key, _}) -> key end)
+    |> Enum.take(5)
+    |> Enum.join
+  end
+
+  test "a-b-c-d-e-f-g-h-987[abcde] is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically" do
+    assert real_room?("a-b-c-d-e-f-g-h-987[abcde]") == true
+  end
+
+  test "not-a-real-room-404[oarel] is a real room" do
+    assert real_room?("not-a-real-room-404[oarel]") == true
+  end
+
+  test "totally-real-room-200[decoy] is not a real room" do
+    assert real_room?("totally-real-room-200[decoy]") == false
+  end
+
+  test "Of the real rooms from the list above, the sum of their sector IDs is 1514" do
+    assert id_sum("aaaaa-bbb-z-y-x-123[abxyz]\na-b-c-d-e-f-g-h-987[abcde]\nnot-a-real-room-404[oarel]\ntotally-real-room-200[decoy]\n") == 1514
+  end
+
+  def id_sum(input) do
+    input
+    |> String.split("\n")
+    |> Enum.slice(0..-2)
+    |> Enum.map(fn(room) -> extract_room_details(room) end)
+    |> Enum.filter(fn({name, _id, checksum}) -> checksum(name) == checksum end)
+    |> Enum.reduce(0, fn({_, id, _}, sum) -> String.to_integer(id) + sum end)
+  end
+
+  test "What is the sum of the sector IDs of the real rooms?" do
+    assert File.read!("test/fixtures/input04.txt")
+    |> id_sum == 185371
+  end
+end
